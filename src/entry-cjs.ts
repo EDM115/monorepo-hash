eb031f4bc6b094a3d9b415780cd47d1619dc9211
EDM115 <github@edm115.dev>
2025-12-05 23:23:55 +0100

    fix: properly bundle the code in the Bun executable

--- src/entry-cjs.ts
+++ src/entry-cjs.ts
@@ -1,72 +1,8 @@
-// oxlint-disable no-require-imports
-const { pathToFileURL } = require("node:url")
-const {
-  PACKAGE_MANAGERS,
-  log,
-  exists,
-  zeroPad,
-  safeExit,
-  mapLimit,
-  getWorkspaceFileList,
-  isPackageManager,
-  detectPNPM,
-  detectDeno,
-  detectPkgJson,
-  autoDetect,
-  detectSpecified,
-  writeDebugFile,
-  loadDebugFile,
-  writeRootDebugFile,
-  loadRootDebugFile,
-  generateDebug,
-  computePerFileHashes,
-  computeOwnHashFromPerFile,
-  computeFinalHash,
-  writeRootHashFile,
-  loadRootHashFile,
-  generateHashes,
-  compareHashes,
-  hash,
-  runCli,
-} = require("./monorepo-hash")
+import { runCli } from "./monorepo-hash"
 
-// Auto-run only when executed as the main entry point (not when required)
-if (pathToFileURL(process.argv[1] ?? "").href === pathToFileURL(__filename).href) {
-  (async () => {
-    await runCli()
-  })()
-    .catch((error) => {
-      console.error(error)
-    })
-}
-
-module.exports = {
-  PACKAGE_MANAGERS,
-  log,
-  exists,
-  zeroPad,
-  safeExit,
-  mapLimit,
-  getWorkspaceFileList,
-  isPackageManager,
-  detectPNPM,
-  detectDeno,
-  detectPkgJson,
-  autoDetect,
-  detectSpecified,
-  writeDebugFile,
-  loadDebugFile,
-  writeRootDebugFile,
-  loadRootDebugFile,
-  generateDebug,
-  computePerFileHashes,
-  computeOwnHashFromPerFile,
-  computeFinalHash,
-  writeRootHashFile,
-  loadRootHashFile,
-  generateHashes,
-  compareHashes,
-  hash,
-  runCli,
-  "default": runCli,
-}
+(async () => {
+  await runCli()
+})()
+  .catch((error: unknown) => {
+    console.error(error)
+  })
